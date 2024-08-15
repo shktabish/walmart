6a9be763d2571405a9f84e17a82b38255b535718
shreyarathod <shreyarathod2305@gmail.com>
2024-08-15 17:48:01 +0530

    kiya kuch toh

--- frontend/src/context/ChatContext.jsx
+++ frontend/src/context/ChatContext.jsx
@@ -1,9 +1,17 @@
-import React, { createContext, useState, useContext } from 'react';
+import React, { createContext, useState, useContext, useEffect } from 'react';
 
 const ChatContext = createContext();
 
 export const ChatProvider = ({ children }) => {
-  const [chatId, setChatId] = useState(null);
+  const [chatId, setChatId] = useState("chat");
+
+    useEffect(() => {
+      const currentUrl = window.location.href;
+      const parts = currentUrl.split('/');
+      const Id = parts.pop();
+      setChatId(Id);
+    }, []);
+
 
   return (
     <ChatContext.Provider value={{ chatId, setChatId }}>
